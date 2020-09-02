9c626f9b4e12fa5a7899fc8b8e4e5c9b0e3053a2
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-09-02 13:27:25 +0100

    added test runtime classpath for smoketest

--- src/smokeTest/java/uk/gov/hmcts/reform/DemoSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/DemoSmokeTest.java
@@ -1,8 +1,8 @@
 package uk.gov.hmcts.reform;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DemoSmokeTest {
     @Test
