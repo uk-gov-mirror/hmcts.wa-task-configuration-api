296931dcbe9e88fef4d188c2ce87a668a8a69361
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-09-03 15:25:37 +0100

    Added simple welcome test for functional tests

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskconfigurationapi/WelcomeTest.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.reform.wataskconfigurationapi;
+
+import static org.hamcrest.Matchers.containsString;
+
+import io.restassured.RestAssured;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import net.serenitybdd.rest.SerenityRest;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+
+@RunWith(SpringIntegrationSerenityRunner.class)
+@SpringBootTest
+@ActiveProfiles("functional")
+public class WelcomeTest {
+
+    @Value("${targetInstance}") private String targetInstance;
+
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = targetInstance;
+        RestAssured.useRelaxedHTTPSValidation();
+    }
+
+    @Test
+    public void should_welcome_with_200_response_code() {
+
+        SerenityRest.given()
+            .when()
+            .get("/")
+            .then()
+            .statusCode(HttpStatus.OK.value())
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(containsString("Welcome to Immigration & Asylum case API"));
+    }
+}
