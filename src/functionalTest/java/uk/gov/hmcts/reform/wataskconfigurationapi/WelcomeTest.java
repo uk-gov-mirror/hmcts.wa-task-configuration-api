e8a7a65998adef106f14c568f1c6e0adf4d556a4
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2020-09-03 15:53:56 +0100

    Fixed incorrect content type. Fixed test throwing connection refused due to port not being configured

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskconfigurationapi/WelcomeTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskconfigurationapi/WelcomeTest.java
@@ -10,20 +10,28 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 
 @RunWith(SpringIntegrationSerenityRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("functional")
 public class WelcomeTest {
 
-    @Value("${targetInstance}") private String targetInstance;
+    @Value("${targetInstance}") private String testUrl;
+    @LocalServerPort
+    private int port;
 
     @Before
     public void setUp() {
-        RestAssured.baseURI = targetInstance;
+
+        if (testUrl.equals("http://localhost")) {
+            RestAssured.port = port;
+        } else {
+            RestAssured.baseURI = testUrl;
+        }
         RestAssured.useRelaxedHTTPSValidation();
     }
 
