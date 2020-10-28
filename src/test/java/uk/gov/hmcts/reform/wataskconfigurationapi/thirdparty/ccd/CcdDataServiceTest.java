c557ad52eaa54d21baba40a46745491f7c3971ff
Ben Nzewi <Benster.nzewi@gmail.com>
2020-10-28 12:42:31 +0000

    Increase test coverage for thirdparty package (#51)
    * Increase test coverage for thirdparty package
    
    * fix checkstyle issues
    
    Co-authored-by: bensternzewi <benster.nzewi@hmcts.net>
    Co-authored-by: Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
    Co-authored-by: david.crespo <david.crespo@hmcts.net>

---
+++ src/test/java/uk/gov/hmcts/reform/wataskconfigurationapi/thirdparty/ccd/CcdDataServiceTest.java
@@ -0,0 +1,62 @@
+package uk.gov.hmcts.reform.wataskconfigurationapi.thirdparty.ccd;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
+import uk.gov.hmcts.reform.wataskconfigurationapi.thirdparty.idam.IdamApi;
+import uk.gov.hmcts.reform.wataskconfigurationapi.thirdparty.idam.IdamSystemTokenGenerator;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CcdDataServiceTest {
+
+    @Mock
+    CcdClient ccdClient;
+
+    @Mock
+    IdamApi idamApi;
+
+    @Mock
+    AuthTokenGenerator authTokenGenerator;
+
+    @Mock
+    IdamSystemTokenGenerator idamSystemTokenGenerator;
+
+    private CcdDataService ccdDataService;
+
+    @Before
+    public void setUp() {
+        ccdDataService = new CcdDataService(ccdClient, authTokenGenerator, idamSystemTokenGenerator);
+    }
+
+    @Test
+    public void should_get_case_data() {
+        String ccdId = UUID.randomUUID().toString();
+        String userToken = "user_token";
+        String serviceToken = "service_token";
+
+        String caseData = "{ "
+            + "\"jurisdiction\": \"ia\", "
+            + "\"case_type_id\": \"Asylum\", "
+            + "\"security_classification\": \"PUBLIC\","
+            + "\"data\": {}"
+            + " }";
+
+        when(idamSystemTokenGenerator.generate()).thenReturn(userToken);
+        when(authTokenGenerator.generate()).thenReturn(serviceToken);
+
+        when(ccdClient.getCase("Bearer " + userToken, serviceToken, ccdId)).thenReturn(caseData);
+
+        String actualCaseData = ccdDataService.getCaseData(ccdId);
+
+        assertEquals(actualCaseData, caseData);
+    }
+}
