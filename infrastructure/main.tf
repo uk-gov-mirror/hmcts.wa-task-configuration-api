9d722f450924c794c984172e301ef9039642d7a5
Alejandro Baeza <42570659+alexbaeza@users.noreply.github.com>
2020-11-24 13:19:26 +0000

    RWA-233: Fix terraform to pull s2s secret from s2s vault and copy into wa-vault (#72)
    * Fix terraform to pull s2s secret from s2s vault and copy into wa-vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -8,3 +8,19 @@ data "azurerm_key_vault" "wa_key_vault" {
   resource_group_name = "${var.product}-${var.env}"
 }
 
+data "azurerm_key_vault" "s2s_key_vault" {
+  name                = "s2s-${var.env}"
+  resource_group_name = "rpe-service-auth-provider-${var.env}"
+}
+
+data "azurerm_key_vault_secret" "s2s_secret" {
+  key_vault_id = data.azurerm_key_vault.s2s_key_vault.id
+  name        = "microservicekey-wa-task-configuration-api"
+}
+
+resource "azurerm_key_vault_secret" "s2s_secret_task_configuration_api" {
+  name         = "s2s-secret-task-configuration-api"
+  value        = data.azurerm_key_vault_secret.s2s_secret.value
+  key_vault_id = data.azurerm_key_vault.wa_key_vault.id
+}
+
