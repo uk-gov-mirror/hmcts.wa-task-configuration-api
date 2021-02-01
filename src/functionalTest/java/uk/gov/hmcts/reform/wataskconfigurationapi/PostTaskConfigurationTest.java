9711c3c21037006f475e6ac3fd54aff16041b356
lewis150193 <lewis.williams@hmcts.net>
2021-02-01 11:03:03 +0000

    Added functional tests

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskconfigurationapi/PostTaskConfigurationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskconfigurationapi/PostTaskConfigurationTest.java
@@ -92,7 +92,7 @@ public class PostTaskConfigurationTest extends SpringBootFunctionalBaseTest {
             .body("configuration_variables.caseId", equalTo(caseId))
             .body("configuration_variables.securityClassification", equalTo("PUBLIC"))
             .body("configuration_variables.autoAssigned", equalTo(true))
-            .body("configuration_variables.hasWarnings", equalTo(true))
+            .body("configuration_variables.hasWarnings", equalTo(false))
             .body("configuration_variables.taskSystem", equalTo("SELF"));
     }
 
@@ -124,7 +124,7 @@ public class PostTaskConfigurationTest extends SpringBootFunctionalBaseTest {
             .body("configuration_variables.caseId", equalTo(caseId))
             .body("configuration_variables.securityClassification", equalTo("PUBLIC"))
             .body("configuration_variables.autoAssigned", equalTo(false))
-            .body("configuration_variables.hasWarnings", equalTo(true))
+            .body("configuration_variables.hasWarnings", equalTo(false))
             .body("configuration_variables.taskSystem", equalTo("SELF"));
     }
 
