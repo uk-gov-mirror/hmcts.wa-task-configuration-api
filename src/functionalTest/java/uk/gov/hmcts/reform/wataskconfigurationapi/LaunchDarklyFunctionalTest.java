ce2fb64fdf498521f799392c6dca61a6f8fbd513
lewis150193 <35262933+lewis150193@users.noreply.github.com>
2021-03-04 14:23:23 +0000

    Update LaunchDarklyFunctionalTest.java

--- src/functionalTest/java/uk/gov/hmcts/reform/wataskconfigurationapi/LaunchDarklyFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/wataskconfigurationapi/LaunchDarklyFunctionalTest.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.wataskconfigurationapi;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import uk.gov.hmcts.reform.wataskconfigurationapi.util.LaunchDarklyFunctionalTestClient;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -10,17 +9,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class LaunchDarklyFunctionalTest extends SpringBootFunctionalBaseTest {
 
-
-    @Value("${launchDarkly.sdkKey}")
-    private String sdkKey;
-
     @Autowired
     private LaunchDarklyFunctionalTestClient launchDarklyFunctionalTestClient;
 
 
     @Test
     public void should_hit_launch_darkly() {
-        boolean launchDarklyFeature = launchDarklyFunctionalTestClient.getKey(sdkKey);
+        boolean launchDarklyFeature = launchDarklyFunctionalTestClient.getKey("tester");
 
         assertThat(launchDarklyFeature, is(false));
     }
