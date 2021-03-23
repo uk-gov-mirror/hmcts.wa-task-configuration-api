c5b717f0a10942a334493139adf3b96ca792e7d8
lewis150193 <lewis.williams@hmcts.net>
2021-03-23 16:17:28 +0000

    ccd-case-pact-test

--- src/contractTest/java/uk/gov/hmcts/reform/wataskconfigurationapi/SpringBootContractBaseTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskconfigurationapi/SpringBootContractBaseTest.java
@@ -11,14 +11,18 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 public abstract class SpringBootContractBaseTest {
 
+
+    public static final String EXPERIMENTAL = "true";
     public static final String AUTHORIZATION_BEARER_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJraWQiOiJiL082T3ZWdeRre";
     public static final String SERVICE_BEARER_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJ6aXAiOiJOT05FIiwia2lkIjoiYi9PNk92V";
+
 
 
     public HttpHeaders getHttpHeaders() {
         HttpHeaders headers = new HttpHeaders();
         headers.add("ServiceAuthorization", SERVICE_BEARER_TOKEN);
         headers.add("Authorization", AUTHORIZATION_BEARER_TOKEN);
+        headers.add("Experimental", EXPERIMENTAL);
         return headers;
     }
 }
