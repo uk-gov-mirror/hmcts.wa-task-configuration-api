874540d2d76128569d88255f0a7cf7240a84af8e
lewis150193 <lewis.williams@hmcts.net>
2021-03-26 09:13:34 +0000

    idam pact-test

--- src/contractTest/java/uk/gov/hmcts/reform/wataskconfigurationapi/SpringBootContractBaseTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/wataskconfigurationapi/SpringBootContractBaseTest.java
@@ -12,17 +12,20 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 public abstract class SpringBootContractBaseTest {
 
 
-    public static final String EXPERIMENTAL = "true";
     public static final String AUTHORIZATION_BEARER_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJraWQiOiJiL082T3ZWdeRre";
     public static final String SERVICE_BEARER_TOKEN = "Bearer eyJ0eXAiOiJKV1QiLCJ6aXAiOiJOT05FIiwia2lkIjoiYi9PNk92V";
-
+    public static final String PACT_TEST_EMAIL_VALUE = "ia-caseofficer@fake.hmcts.net";
+    public static final String PACT_TEST_PASSWORD_VALUE = "London01";
+    public static final String PACT_TEST_CLIENT_ID_VALUE = "pact";
+    public static final String PACT_TEST_CLIENT_SECRET_VALUE = "pactsecret";
+    public static final String PACT_TEST_SCOPES_VALUE = "openid profile roles";
+    public static final String PACT_TEST_ROLES_VALUE = "caseworker";
 
 
     public HttpHeaders getHttpHeaders() {
         HttpHeaders headers = new HttpHeaders();
         headers.add("ServiceAuthorization", SERVICE_BEARER_TOKEN);
         headers.add("Authorization", AUTHORIZATION_BEARER_TOKEN);
-        headers.add("Experimental", EXPERIMENTAL);
         return headers;
     }
 }
