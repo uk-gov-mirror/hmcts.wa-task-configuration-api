d4e398074df9cfd6d55e61b4868bd6c7972946e7
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-04-13 13:21:21 +0100

    fix it test

--- src/integrationTest/java/uk/gov/hmcts/reform/wataskconfigurationapi/controllers/GetRootTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/wataskconfigurationapi/controllers/GetRootTest.java
@@ -16,7 +16,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @WebMvcTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 @ContextConfiguration(classes = {RootController.class})
 @ActiveProfiles("integration")
 class GetRootTest {
