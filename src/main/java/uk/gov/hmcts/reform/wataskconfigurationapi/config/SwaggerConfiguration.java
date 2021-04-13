c1f851f73426650284fb5c6fa6dfa6e2998cfba1
Alejandro Baeza <alejandro.baeza@enginegroup.com>
2021-04-13 11:38:03 +0100

    Update swagger to v3 added require s2s headers configuration and functional test to avoid potential issues when updating via dependabot

--- src/main/java/uk/gov/hmcts/reform/wataskconfigurationapi/config/SwaggerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/wataskconfigurationapi/config/SwaggerConfiguration.java
@@ -5,10 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.RequestParameterBuilder;
+import springfox.documentation.service.ParameterType;
+import springfox.documentation.service.RequestParameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import uk.gov.hmcts.reform.wataskconfigurationapi.Application;
+
+import static java.util.Collections.singletonList;
 
 @Configuration
 @EnableSwagger2
@@ -27,6 +32,18 @@ public class SwaggerConfiguration {
             .select()
             .apis(RequestHandlerSelectors.basePackage(Application.class.getPackage().getName() + ".controllers"))
             .paths(PathSelectors.any())
+            .build()
+            .globalRequestParameters(singletonList(
+                headerServiceAuthorization()
+            ));
+    }
+
+    private RequestParameter headerServiceAuthorization() {
+        return new RequestParameterBuilder()
+            .name("ServiceAuthorization")
+            .description("Keyword `Bearer` followed by a service-to-service token for a whitelisted micro-service")
+            .in(ParameterType.HEADER)
+            .required(true)
             .build();
     }
 
